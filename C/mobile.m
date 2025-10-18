e3d094ab8049454d5090d511ba8592d6acb4158c
Macint128 <albert0424@kakao.com>
2025-10-18 15:24:18 +0900

    Create mobile.m to log a message

---
+++ C/mobile.m
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello from signed Unix executable!");
+    }
+    return 0;
+}
